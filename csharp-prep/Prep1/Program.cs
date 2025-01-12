bd847cb81aaa1041f129a2811ceac8bfcd2bda74
Nathan Howarth <bundo315.nathan@gmail.com>
2025-01-11 21:15:31 -0700

    pushed code to github

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
 
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();  //line to input first name  
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();  //line to input last name
+        
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
     }
 }
