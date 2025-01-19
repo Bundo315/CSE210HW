fb6c42416685f930759121c198d165ed1fe2ba8d
Nathan Howarth <bundo315.nathan@gmail.com>
2025-01-18 17:30:14 -0700

    submitted prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        int count = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+                count = count + 1;
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+                count = count + 1;
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {count} guesses.");
+            }
+
+        }                    
     }
 }
